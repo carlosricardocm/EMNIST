b84bbd85f72fe010aa55e91f0d2d9d67fcaaeabd
carlosricardocm <ricardo_cm@hotmail.com>
2023-06-06 19:08:50 -0600

    cambios

--- SMAC_run_sameconfigforall.py
+++ SMAC_run_sameconfigforall.py
@@ -360,7 +360,7 @@ def optimize():
     #"wallclock_limit": 7*86400, #863400 = 24hrs = 2 *  12 hrs = 12*60*60 secs
     #"n-workers":32,  # Use 32 workers
     "deterministic":"true", # hace que solo 1 semilla se pruebe en cada ejecucion de la funcion objetivo
-    "runcount-limit": 5, #Num evaluations
+    "runcount-limit": 1, #Num evaluations
     "n_trials":"1",  # Evaluated max 500 trials
     })
 
--- main_test_associative.py
+++ main_test_associative.py
@@ -1374,7 +1374,7 @@ def main(action, training_stage):#, occlusion = None, bar_type= None, tolerance 
         training_percentage = constants.nn_training_percent
         model_prefix = constants.model_name
         stats_prefix = constants.stats_model_name 
-        constants.training_stage = traning.stage      
+        constants.training_stage = training_stage      
 
         history = convnet.train_networks(training_percentage, model_prefix, action)
         save_history(history, training_stage, stats_prefix)
